25c6e4ca56da373845aea82662398b002d6eacea
Montana McCallum <montana.mccallum@gmail.com>
2019-04-04 00:36:42 +0000

    Tute 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+
+  get 'static_pages/home'
+
+  get '/help', to: 'static_pages#help', as: 'helf'
+  
+  get '/about', to: 'static_pages#about'
+  
+  get  '/contact', to: 'static_pages#contact'
+
+  root 'static_pages#home'
 end
